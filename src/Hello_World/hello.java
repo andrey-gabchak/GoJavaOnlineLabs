e087460bf2529c1ab850249cdcf3d275be18bb4f
andrey-gabchak <andrey.gabchak@gmail.com>
2016-02-25 23:10:56 +0200

    Hello World!

---
+++ src/Hello_World/hello.java
@@ -0,0 +1,10 @@
+package Hello_World;
+
+/**
+ * Created by Andrey on 25.02.2016.
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
