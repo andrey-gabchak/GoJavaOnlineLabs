5e4399d551bc406f27c244794dd6d78dbde01c03
andrey-gabchak <andrey.gabchak@gmail.com>
2016-02-26 12:18:14 +0200

    Выполнил ДЗ к модулю 1.

---
+++ src/Hello_World/FirstProgramm.java
@@ -0,0 +1,13 @@
+package Hello_World;
+
+/**
+ * Created by Andrey on 26.02.2016.
+ * Написать, скомпилировать и запустить из консоли программу,
+ * печатающую на экран сообщение: "Hi, I'm your first Java program! Stay tuned!"
+ */
+public class FirstProgramm {
+
+    public static void main (String[] args) {
+        System.out.println("Hi, I'm your first Java program! Stay tuned!");
+    }
+}
