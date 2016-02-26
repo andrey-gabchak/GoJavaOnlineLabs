8732fd1767593d2898a76f96fb81ca126630f031
andrey-gabchak <andrey.gabchak@gmail.com>
2016-02-26 16:21:28 +0200

    Исправил UML диаграмму Музыкальные инструменты

--- src/Hello_World/hello.java
+++ src/Hello_World/hello.java
@@ -4,7 +4,7 @@ package Hello_World;
  * Created by Andrey on 25.02.2016.
  * Программа должна выводить на экран фразу "Hello World!".
  */
-class Hello {
+class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
