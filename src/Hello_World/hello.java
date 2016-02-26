f8fb5038432ebda6766a59cf60d5cc43ec296b84
andrey-gabchak <andrey.gabchak@gmail.com>
2016-02-26 12:03:59 +0200

    Внес исправления согласно Java Code Conventions.

--- src/Hello_World/hello.java
+++ src/Hello_World/hello.java
@@ -2,8 +2,10 @@ package Hello_World;
 
 /**
  * Created by Andrey on 25.02.2016.
+ * Программа должна выводить на экран фразу "Hello World!".
  */
-public class hello {
+public class Hello {
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
