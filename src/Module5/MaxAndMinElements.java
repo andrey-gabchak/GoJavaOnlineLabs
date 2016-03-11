c69a6b05c5185068d2d3273ccc5b2a5f9a9da486
andrey.gabchak@gmail.com <andrey.gabchak@gmail.com>
2016-03-11 17:39:51 +0200

    Max and min elements in array. Version 1

---
+++ src/Module5/MaxAndMinElements.java
@@ -0,0 +1,46 @@
+package Module5;
+
+/**
+ * class MaxAndMinElements
+ *
+ * Create a class that implements the search for the maximum and minimum elements
+ * in an array of integers (int []).
+ *
+ * Created by coura on 11.03.2016.
+ */
+
+public class MaxAndMinElements {
+
+    public static void main(String[] args) {
+
+        int[] intArray = {0, 1, 2, 4, 10, 6, 1};
+
+        System.out.println(Minimum(intArray));
+        System.out.println(Maximum(intArray));
+
+    }
+
+    private static int Minimum (int Array[]) {
+        int minVariable = Array[0];
+
+        for (int count = 0; count < (Array.length - 1); count++) {
+
+            if (minVariable > Array[count + 1]) {
+                minVariable = Array[count + 1];
+            }
+        }
+        return minVariable;
+    }
+
+    private static int Maximum (int Array[]) {
+        int maxVarible = Array[0];
+
+        for (int count = 0; count < (Array.length - 1); count++) {
+
+            if (maxVarible < Array[count + 1]) {
+                maxVarible = Array[count + 1];
+            }
+        }
+        return maxVarible;
+    }
+}
