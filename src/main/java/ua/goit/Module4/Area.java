b1ec2cb54373f8fe2decdf985bada9ecb6a90a46
andrey.gabchak@gmail.com <andrey.gabchak@gmail.com>
2016-04-09 21:34:45 +0300

    Refactor for unit-tests

--- src/main/java/ua/goit/Module4/Area.java
+++ src/main/java/ua/goit/Module4/Area.java
@@ -12,6 +12,7 @@ import java.io.InputStreamReader;
  * Created by coura on 07.03.2016.
  */
 
+
 public class Area {
 
     public static void main(String[] args) throws Exception {
@@ -25,17 +26,32 @@ public class Area {
 
         // calculate the area of a triangle
         if (figure == 1) {
-            double triangleResult = triagleValidation();
+            System.out.println("Enter the sides of triangle:");
+            BufferedReader triangleReader = new BufferedReader(new InputStreamReader(System.in));
+            String side1 = triangleReader.readLine();
+            String side2 = triangleReader.readLine();
+            String side3 = triangleReader.readLine();
+
+            double triangleResult = triagleArea(side1, side2, side3);
             if (triangleResult > 0) System.out.println("The area of a triangle is: " + triangleResult);
 
 
         } else if (figure == 2) { // calculate the area of a rectangle
-            int rectangleResult = rectangleArea();
+            System.out.println("Enter the sides of rectangle:");
+            BufferedReader rectangleReader = new BufferedReader(new InputStreamReader(System.in));
+            String side1 = rectangleReader.readLine();
+            String side2 = rectangleReader.readLine();
+
+            int rectangleResult = rectangleArea(side1, side2);
             if (rectangleResult > 0) System.out.println("The area is: " + rectangleResult);
 
 
         } else if (figure == 3) { // the area of a circle
-            double circleResult = circleArea();
+            System.out.println("Enter the radius of circle:");
+            BufferedReader circleReader = new BufferedReader(new InputStreamReader(System.in));
+            String radius = circleReader.readLine();
+
+            double circleResult = circleArea(radius);
             if (circleResult > 0) System.out.println("Area of the circle is: " + circleResult);
 
         } else {
@@ -53,85 +69,50 @@ public class Area {
         }
     }
 
-    public static double triagleValidation() throws IOException {
-        System.out.print("Enter the first side of the triangle size: ");
-        BufferedReader triangle = new BufferedReader(new InputStreamReader(System.in));
-        String sSide1 = triangle.readLine();
+    public static double triagleArea(String sSide1, String sSide2, String sSide3) {
 
-        if (isPositiveInteger(sSide1)) {
+        if ((isPositiveInteger(sSide1)) && (isPositiveInteger(sSide2)) && (isPositiveInteger(sSide3))) {
             int side1 = Integer.parseInt(sSide1);
-            System.out.print("Enter the size of the second side of the triangle: ");
-            String sSide2 = triangle.readLine();
+            int side2 = Integer.parseInt(sSide2);
+            int side3 = Integer.parseInt(sSide3);
 
-            if (isPositiveInteger(sSide2)) {
-                int side2 = Integer.parseInt(sSide2);
-                System.out.print("Enter the third side of the triangle size: ");
-                String sSide3 = triangle.readLine();
+            int summSide1 = side1 + side2;
+            int summSide2 = side1 + side3;
+            int summSide3 = side2 + side3;
 
-                if (isPositiveInteger(sSide3)) {
-                    int side3 = Integer.parseInt(sSide3);
+            if ((summSide1 > side3) && (summSide2 > side2) && (summSide3 > side1)) {
 
-                    return triangleFormula(side1, side2, side3);
-                }
+                double perimeter = (side1 + side2 + side3) / 2.0;
+
+                return Math.sqrt(perimeter * (perimeter - side1) *
+                        (perimeter - side2) * (perimeter - side3));
+            } else {
+                System.out.println("[Error] Triangle with such sides do not exist. " +
+                        "The sum of any two sides must be greater than the third to the rectangle exist!");
+                return 0;
             }
         }
         return 0;
     }
 
-    public static Double triangleFormula(int side1, int side2, int side3) {
-        int summSide1 = side1 + side2;
-        int summSide2 = side1 + side3;
-        int summSide3 = side2 + side3;
+    public static int rectangleArea(String sFirstSide, String sSecondSide) throws IOException {
 
-        if ((summSide1 > side3) && (summSide2 > side2) && (summSide3 > side1)) {
-
-            double perimeter = (side1 + side2 + side3) / 2.0;
-
-            return Math.sqrt(perimeter * (perimeter - side1) *
-                    (perimeter - side2) * (perimeter - side3));
-
-        }
-        return 0.0;
-    }
-
-    public static int rectangleArea() throws IOException {
-        System.out.println("Enter the dimensions of the rectangle: ");
-
-        BufferedReader rectangle = new BufferedReader(new InputStreamReader(System.in));
-        String sFirstSide = rectangle.readLine();
-        String sSecondSide = rectangle.readLine();
-
-        if (isPositiveInteger(sFirstSide)) {
+        if (isPositiveInteger(sFirstSide) && isPositiveInteger(sSecondSide)) {
             int firstSide = Integer.parseInt(sFirstSide);
+            int secondSide = Integer.parseInt(sSecondSide);
 
-            if (isPositiveInteger(sSecondSide)) {
-                int secondSide = Integer.parseInt(sSecondSide);
-
-                return rectangleFormula(firstSide, secondSide);
-            }
+            return firstSide * secondSide;
         }
         return 0;
     }
 
-    public static int rectangleFormula(int firstSide, int secondSide) {
-        return firstSide * secondSide;
-    }
-
-    public static double circleArea() throws IOException {
-        System.out.print("Enter the radius of the circle: ");
-
-        BufferedReader circle = new BufferedReader(new InputStreamReader(System.in));
-        String sRadius = circle.readLine();
+    public static double circleArea(String sRadius) throws IOException {
 
         if (isPositiveInteger(sRadius)) {
             int radius = Integer.parseInt(sRadius);
 
-            return circleFormula(radius);
+            return Math.PI * Math.pow(radius, 2.0);
         }
         return 0;
-    }
-
-    public static double circleFormula(int radius) {
-        return Math.PI * Math.pow(radius, 2.0);
     }
 }
