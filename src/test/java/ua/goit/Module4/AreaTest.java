6f1033699d8c2a3b668171b0753d20fd1042f036
andrey.gabchak@gmail.com <andrey.gabchak@gmail.com>
2016-04-09 21:33:39 +0300

    Unit-tests for calculation area of figures.

--- src/test/java/ua/goit/Module4/AreaTest.java
+++ src/test/java/ua/goit/Module4/AreaTest.java
@@ -1,31 +1,135 @@
 package ua.goit.Module4;
 
-import static org.junit.Assert.*;
+import com.sun.deploy.util.StringUtils;
 import org.junit.*;
-import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 /**
  * Created by coura on 09.04.2016.
+ *
+ * Unit-tests for calculation area of figures.
  */
 public class AreaTest {
 
-    @Test
-    public void testMain() throws Exception {
+    private static Area area;
 
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+        area = new Area();
+    }
+
+
+    //tests of triangle
+    @Test
+    public void testTriangleAreaPositiveNumbers() throws Exception {
+        String side1 = "3";
+        String side2 = "3";
+        String side3 = "3";
+
+        double result = area.triagleArea(side1, side2, side3);
+
+        Assert.assertEquals(3.897, result, 0.001);
     }
 
     @Test
-    public void testCircleArea() throws Exception {
+    public void testTriangleAreaNegativeNumbers() throws Exception {
+        String side1 = "-3";
+        String side2 = "-3";
+        String side3 = "-3";
 
+        double result = area.triagleArea(side1, side2, side3);
+
+        Assert.assertEquals(0, result, 0.001);
     }
 
     @Test
-    public void testRectangleArea() throws Exception {
+    public void testTriangleAreaNotNumber() throws Exception {
+        String side1 = "-3";
+        String side2 = "a";
+        String side3 = "-3";
 
+        double result = area.triagleArea(side1, side2, side3);
+
+        Assert.assertEquals(0, result, 0.001);
     }
 
     @Test
-    public void testTriagleArea() throws Exception {
+    public void testTriangleAreaNonexistent() throws Exception {
+        String side1 = "1";
+        String side2 = "1";
+        String side3 = "3";
 
+        double result = area.triagleArea(side1, side2, side3);
+
+        Assert.assertEquals(0, result, 0.001);
+    }
+
+    //tests of rectangle
+    @Test
+    public void testRectangleAreaPositiveNumbers() throws Exception {
+        String side1 = "3";
+        String side2 = "3";
+
+        int result = area.rectangleArea(side1, side2);
+
+        assertEquals(9, result);
+    }
+
+    @Test
+    public void testRectangleAreaNegativeNumbers() throws Exception {
+        String side1 = "-3";
+        String side2 = "3";
+
+        int result = area.rectangleArea(side1, side2);
+
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void testRectangleAreaNotNumbers() throws Exception {
+        String side1 = "a";
+        String side2 = "3";
+
+        int result = area.rectangleArea(side1, side2);
+
+        assertEquals(0, result);
+    }
+
+    //tests of circle
+    @Test
+    public void testCircleAreaPositiveNumbers() throws Exception {
+        String radius = "3";
+
+        double result = area.circleArea(radius);
+
+        assertEquals(28.274, result, 0.001);
+    }
+
+    @Test
+    public void testCircleAreaNegativeNumbers() throws Exception {
+        String radius = "-3";
+
+        double result = area.circleArea(radius);
+
+        assertEquals(0, result, 0.001);
+    }
+
+    @Test
+    public void testCircleAreaNotNumbers() throws Exception {
+        String radius = "a";
+
+        double result = area.circleArea(radius);
+
+        assertEquals(0, result, 0.001);
+    }
+
+    @Test
+    public void testCircleAreaNull() throws Exception {
+        String radius = "0";
+
+        double result = area.circleArea(radius);
+
+        assertEquals(0, result, 0.001);
     }
 }
