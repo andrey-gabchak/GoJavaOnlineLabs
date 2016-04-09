dbdc7cad4f279b92f4c9549c1a01a471685df244
andrey.gabchak@gmail.com <andrey.gabchak@gmail.com>
2016-04-09 20:02:10 +0300

    Refactor of validation

--- src/main/java/ua/goit/Module4/Area.java
+++ src/main/java/ua/goit/Module4/Area.java
@@ -48,6 +48,7 @@ public class Area {
             int tmpInteger = Integer.parseInt(number);
             return tmpInteger > 0;
         } catch (NumberFormatException e) {
+            System.out.println("[Error] Must enter the positive integer number!");
             return false;
         }
     }
@@ -57,46 +58,33 @@ public class Area {
         BufferedReader triangle = new BufferedReader(new InputStreamReader(System.in));
         String sSide1 = triangle.readLine();
 
-        try {
-            if (isPositiveInteger(sSide1)) {
-                int side1 = Integer.parseInt(sSide1);
-                System.out.print("Enter the size of the second side of the triangle: ");
-                String sSide2 = triangle.readLine();
+        if (isPositiveInteger(sSide1)) {
+            int side1 = Integer.parseInt(sSide1);
+            System.out.print("Enter the size of the second side of the triangle: ");
+            String sSide2 = triangle.readLine();
 
-                if (isPositiveInteger(sSide2)) {
-                    int side2 = Integer.parseInt(sSide2);
-                    System.out.print("Enter the third side of the triangle size: ");
-                    String sSide3 = triangle.readLine();
+            if (isPositiveInteger(sSide2)) {
+                int side2 = Integer.parseInt(sSide2);
+                System.out.print("Enter the third side of the triangle size: ");
+                String sSide3 = triangle.readLine();
 
-                    if (isPositiveInteger(sSide3)) {
-                        int side3 = Integer.parseInt(sSide3);
+                if (isPositiveInteger(sSide3)) {
+                    int side3 = Integer.parseInt(sSide3);
 
-                        int summSide1 = side1 + side2;
-                        int summSide2 = side1 + side3;
-                        int summSide3 = side2 + side3;
+                    int summSide1 = side1 + side2;
+                    int summSide2 = side1 + side3;
+                    int summSide3 = side2 + side3;
 
-                        if ((summSide1 > side3) && (summSide2 > side2) && (summSide3 > side1)) {
+                    if ((summSide1 > side3) && (summSide2 > side2) && (summSide3 > side1)) {
 
-                            double perimeter = (side1 + side2 + side3) / 2.0;
+                        double perimeter = (side1 + side2 + side3) / 2.0;
 
-                            return Math.sqrt(perimeter * (perimeter - side1) *
-                                    (perimeter - side2) * (perimeter - side3));
+                        return Math.sqrt(perimeter * (perimeter - side1) *
+                                (perimeter - side2) * (perimeter - side3));
 
-                        } else {
-                            System.out.println("Triangle with such parties does not exist!");
-                            return 0;
-                        }
-                    } else {
-                        throw new IOException(sSide3);
                     }
-                } else {
-                    throw new IOException(sSide2);
                 }
-            } else {
-                throw new IOException(sSide1);
             }
-        } catch (Exception e) {
-            System.out.println("Side must be positive integer number!");
         }
         return 0;
     }
@@ -113,10 +101,6 @@ public class Area {
             int secondSide = Integer.parseInt(sSecondSide);
 
             return firstSide * secondSide;
-        } else try {
-            throw new IOException(sFirstSide);
-        } catch (IOException e) {
-            System.out.println("Side must be positive integer number!");
         }
         return 0;
     }
@@ -130,10 +114,6 @@ public class Area {
         if (isPositiveInteger(sRadius)) { // calculate the area of a circle the radius
             int radius = Integer.parseInt(sRadius);
             return Math.PI * Math.pow(radius, 2.0);
-        } else try {
-            throw new IOException(sRadius);
-        } catch (IOException e) {
-            System.out.println("The radius must be positive integer number!");
         }
         return 0;
     }
