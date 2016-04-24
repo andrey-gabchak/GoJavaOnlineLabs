6d7be79d216af99f390ef12f73ef1f38bf73eb4b
andrey.gabchak@gmail.com <andrey.gabchak@gmail.com>
2016-04-24 19:29:02 +0300

    Connect array of characters in a number. Tests

---
+++ src/test/java/ua/goit/Practice1/FindMaxNumberInArrayTest.java
@@ -0,0 +1,23 @@
+package ua.goit.Practice1;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by coura on 24.04.2016.
+ */
+public class FindMaxNumberInArrayTest {
+
+    @Test
+    public void testMax() throws Exception {
+        int[] integers = {100, 1, -2, 4, 99};
+        int expected = 100;
+        FindMaxNumberInArray findMaxNumberInArray = new FindMaxNumberInArray();
+        int result = findMaxNumberInArray.max(integers);
+
+        assertEquals(expected, result);
+    }
+}
