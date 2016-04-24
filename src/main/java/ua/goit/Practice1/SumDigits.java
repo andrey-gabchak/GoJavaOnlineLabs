5101ed2b06d8ec03af57b0fc006679e4cc27aa2f
andrey.gabchak@gmail.com <andrey.gabchak@gmail.com>
2016-04-24 19:25:50 +0300

    Calculate the sum of digits. For example the sum of digits of 123 is equal to 6.

--- src/main/java/ua/goit/Practice1/SumDigits.java
+++ src/main/java/ua/goit/Practice1/SumDigits.java
@@ -1,10 +1,21 @@
 package ua.goit.Practice1;
 
+import java.util.ArrayList;
+
 /**
  * Created by coura on 24.04.2016.
+ *
+ * Calculate the sum of digits.
+ * For example the sum of digits of 123 is equal to 6.
  */
+
 public class SumDigits {
     public int sum(int number) {
-
+        char[] numberToCharArray = String.valueOf(number).toCharArray();
+        int result = 0;
+        for (int i = 0; i < numberToCharArray.length; i++) {
+            if (numberToCharArray[i] != '-') result = result + (numberToCharArray[i] - '0');
+        }
+        return result;
     }
 }
