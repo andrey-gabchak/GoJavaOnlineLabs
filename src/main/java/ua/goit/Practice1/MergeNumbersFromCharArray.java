ede5fa9c203fea6580751b7cb6e8858dffeef775
andrey.gabchak@gmail.com <andrey.gabchak@gmail.com>
2016-04-24 17:31:09 +0300

    Connect array of characters in a number.

---
+++ src/main/java/ua/goit/Practice1/MergeNumbersFromCharArray.java
@@ -0,0 +1,15 @@
+package ua.goit.Practice1;
+
+/**
+ * Created by coura on 24.04.2016.
+ */
+public class MergeNumbersFromCharArray {
+
+    public int join(char[] input) {
+        int result = 0;
+        for (int i = 0; i < input.length; i++) {
+            result = result * 10 + (input[i] - '0');
+        }
+        return result;
+    }
+}
---
+++ src/main/java/ua/goit/Practice1/SumDigits.java
@@ -0,0 +1,10 @@
+package ua.goit.Practice1;
+
+/**
+ * Created by coura on 24.04.2016.
+ */
+public class SumDigits {
+    public int sum(int number) {
+
+    }
+}
