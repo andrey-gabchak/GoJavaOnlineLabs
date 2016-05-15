e76e55d4bd940602c349c135082ff5b51f4c64bb
andrey.gabchak@gmail.com <andrey.gabchak@gmail.com>
2016-05-15 15:54:06 +0300

    Implementation using stringBuilder

--- src/main/java/ua/goit/Practice2/AddBinary.java
+++ src/main/java/ua/goit/Practice2/AddBinary.java
@@ -12,7 +12,7 @@ public class AddBinary {
     public static void main(String[] args) {
 
     }
-// implementation with BigInteger
+// implementation using a BigInteger
 //    String add(String a, String b) {
 //
 //        BigInteger aBig = new BigInteger(a, 2);
@@ -25,7 +25,7 @@ public class AddBinary {
 //        return sResult;
 //    }
 
-    //Implementation with cycle
+    //Implementation using cycle and charArray
     String add(String a, String b) {
         char[] aArray = a.toCharArray();
         char[] bArray = b.toCharArray();
@@ -40,7 +40,6 @@ public class AddBinary {
 
         return result.toString();
     }
-
     private StringBuilder addBinary(char[] aArray, char[] bArray) {
         StringBuilder result = new StringBuilder();
 
@@ -98,5 +97,56 @@ public class AddBinary {
             }
         }
         return result;
+    }
+
+    //Implementation using stringBuilder
+    String addString(String a, String b) {
+        a = new StringBuilder(a).reverse().toString();
+        b = new StringBuilder(b).reverse().toString();
+        int lengthOfMaxString;
+        String stringOfMaxLength;
+
+        if (a.length() > b.length()) {
+            lengthOfMaxString = a.length();
+            stringOfMaxLength = a;
+        } else {
+            lengthOfMaxString = b.length();
+            stringOfMaxLength = b;
+        }
+
+        int remainder = 0;
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < lengthOfMaxString; i++) {
+            if (i < a.length() && i < b.length()) {
+                if (a.charAt(i) == '1' && b.charAt(i) == '1') {
+                    result.append(remainder);
+                    remainder = 1;
+                } else if (a.charAt(i) == '0' && b.charAt(i) == '0') {
+                    result.append(remainder);
+                    remainder = 0;
+                } else {
+                    if (remainder == 0) {
+                        result.append(1);
+                        remainder = 0;
+                    } else {
+                        result.append(0);
+                        remainder = 1;
+                    }
+                }
+
+
+            } else {
+                if (stringOfMaxLength.charAt(i) == '1' && remainder == 1) {
+                    result.append(0);
+                } else if (stringOfMaxLength.charAt(i) == '0' && remainder == 0) {
+                    result.append(0);
+                } else {
+                    result.append(1);
+                    remainder = 0;
+                }
+            }
+        }
+        if (remainder == 1) result.append(1);
+        return result.reverse().toString();
     }
 }
