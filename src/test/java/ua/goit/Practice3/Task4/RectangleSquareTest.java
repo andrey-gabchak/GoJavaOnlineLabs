0f1ff35f38d8e0099792123f0990239f0da0e37f
andrey.gabchak@gmail.com <andrey.gabchak@gmail.com>
2016-05-21 17:47:51 +0300

    Area of rectangles on the axis X

---
+++ src/test/java/ua/goit/Practice3/Task4/RectangleSquareTest.java
@@ -0,0 +1,39 @@
+package ua.goit.Practice3.Task4;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by coura on 21.05.2016.
+ */
+public class RectangleSquareTest {
+
+    RectangleSquare rectangleSquare = new RectangleSquare();
+
+    @Test
+    public void testMeasureTwoFigure() throws Exception {
+        int expected = 300;
+
+        int[] x = {0, 0};
+        int[] height = {20, 10};
+        int[] weight = {10, 20};
+
+        int result = rectangleSquare.measure(x, height, weight);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testMeasureThreeFigure() throws Exception {
+        int expected = 39;
+
+        int[] x = {1, 2, 4};
+        int[] height = {3, 4, 5};
+        int[] weight = {9, 6, 5};
+
+        int result = rectangleSquare.measure(x, height, weight);
+
+        assertEquals(expected, result);
+    }
+}
