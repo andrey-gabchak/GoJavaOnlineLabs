c2fa370de6f363ba8b033fb7d6857b27f84178bd
andrey.gabchak@gmail.com <andrey.gabchak@gmail.com>
2016-06-05 23:07:25 +0300

    Added call of swing interface

--- src/main/java/ua/goit/Enterprise/Module1/CollectionImplementation/Main.java
+++ src/main/java/ua/goit/Enterprise/Module1/CollectionImplementation/Main.java
@@ -1,5 +1,7 @@
 package ua.goit.Enterprise.Module1.CollectionImplementation;
 
+import ua.goit.Enterprise.Module1.CollectionImplementation.GUI.MainWindow;
+
 /**
  * Created by coura on 30.05.2016.
  *
@@ -12,5 +14,7 @@ public class Main {
     public static void main(String[] args) {
         WriteToFile writingToFile = new WriteToFile();
         writingToFile.writeTable();
+        MainWindow mainWindow = new MainWindow();
+        mainWindow.createGUI();
     }
 }
