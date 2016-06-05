086b1712523645ebb2390fb6f4657ebead5c8b7d
andrey.gabchak@gmail.com <andrey.gabchak@gmail.com>
2016-06-05 17:24:04 +0300

    Main method. What else to say?

--- src/main/java/ua/goit/Enterprise/Module1/CollectionImplementation/Logging.java
+++
@@ -1,7 +0,0 @@
-package ua.goit.Enterprise.Module1.CollectionImplementation;
-
-/**
- * Created by coura on 30.05.2016.
- */
-public class Logging {
-}
---
+++ src/main/java/ua/goit/Enterprise/Module1/CollectionImplementation/Main.java
@@ -0,0 +1,16 @@
+package ua.goit.Enterprise.Module1.CollectionImplementation;
+
+/**
+ * Created by coura on 30.05.2016.
+ *
+ * The task in file Task.png
+ */
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        WriteToFile writingToFile = new WriteToFile();
+        writingToFile.writeTable();
+    }
+}
