f60333679408a6ab670b653eaf6a10e9ce4ff1e5
andrey.gabchak@gmail.com <andrey.gabchak@gmail.com>
2016-06-06 22:02:54 +0300

    Changed generics for addTask(Task task, Validator validator) method.

--- src/main/java/ua/goit/Enterprise/Module2/Interfaces/Executor.java
+++ src/main/java/ua/goit/Enterprise/Module2/Interfaces/Executor.java
@@ -11,13 +11,13 @@ public interface Executor<T> {
 
     // Добавить таск на выполнение. Результат таска будет доступен через метод getValidResults().
     // Бросает Эксепшн если уже был вызван метод execute()
-    void addTask(Task<? extends Task> task) throws Exception;
+    void addTask(Task<? extends T> task) throws Exception;
 
     // Добавить таск на выполнение и валидатор результата. Результат таска будет записан в ValidResults
     // если validator.isValid вернет true для этого результата
     // Результат таска будет записан в InvalidResults если validator.isValid вернет false для этого результата
     // Бросает Эксепшн если уже был вызван метод execute()
-    void addTask(Task<? extends Task> task, Validator<? super Validator> validator) throws Exception;
+    void addTask(Task<? extends T> task, Validator<? super T> validator) throws Exception;
 
     // Выполнить все добавленые таски
     void execute();
