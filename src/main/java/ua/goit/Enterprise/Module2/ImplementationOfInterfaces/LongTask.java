241211c20f54bb7edbd346daf37211ff8e20413e
andrey.gabchak@gmail.com <andrey.gabchak@gmail.com>
2016-06-06 21:54:22 +0300

    Implementation of interface Task by Long type.

---
+++ src/main/java/ua/goit/Enterprise/Module2/ImplementationOfInterfaces/LongTask.java
@@ -0,0 +1,20 @@
+package ua.goit.Enterprise.Module2.ImplementationOfInterfaces;
+
+import ua.goit.Enterprise.Module2.Interfaces.Task;
+
+/**
+ * Created by coura on 06.06.2016.
+ */
+public class LongTask implements Task<Long> {
+    private Long result;
+
+    // Метода запускает таск на выполнение
+    public void execute() {
+        System.out.println(getResult());
+    }
+
+    // Возвращает результат выполнения
+    public Long getResult() {
+        return result;
+    }
+}
