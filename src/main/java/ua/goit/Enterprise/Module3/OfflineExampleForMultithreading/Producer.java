376828b80c28563aba9b7822028c41073111430b
andrey.gabchak@gmail.com <andrey.gabchak@gmail.com>
2016-06-13 15:34:32 +0300

    That is a example task which we solved on offline lesson.

--- src/main/java/ua/goit/Enterprise/Module3/OfflineExampleForMultithreading/Producer.java
+++ src/main/java/ua/goit/Enterprise/Module3/OfflineExampleForMultithreading/Producer.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ua.goit.Enterprise.Module3.OfflineExempleForMultithreading;
+package ua.goit.Enterprise.Module3.OfflineExampleForMultithreading;
 
 /**
  *
