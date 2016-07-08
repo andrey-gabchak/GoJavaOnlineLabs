c2d1a65cea7371cf04f37d3521c524b77736a016
andrey.gabchak@gmail.com <andrey.gabchak@gmail.com>
2016-07-08 18:27:50 +0300

    Main method for starting of program.

---
+++ src/main/java/ua/goit/Enterprise/Module3/Part2/Main.java
@@ -0,0 +1,22 @@
+package ua.goit.Enterprise.Module3.Part2;
+
+import java.util.Random;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.IntStream;
+
+/**
+ * Created by coura on 07.07.2016.
+ */
+
+public class Main {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        Random random = new Random(10);
+        int[] values = new int[10];
+        IntStream.range(0, 10).forEach(i -> values[i] = random.nextInt(20));
+        IntStream.range(0, 10).forEach(i -> System.out.print(values[i] + " "));
+
+        System.out.println();
+        System.out.println(new SquareSumRealization().getSquareSum(values, 3));
+    }
+}
